b49e02ee4c68ea4bd7d8c8e2e6f73ec96658ad4e
Kadri-abderrahim <kdri.abderrahim@hotmail.com>
2021-12-08 12:18:23 +0100

    redux

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useSelector } from "react-redux"
+import AddTask from './AddTask';
+import ListTask from './ListTask';
+import { useDispatch } from "react-redux";
+import {filterTask} from "./Actions/Action"
+function App() {
+  const dispatch = useDispatch()
+  const handleSelect = e =>{
+    dispatch(filterTask(e.target.id))
+  }
+  return (
+    <div className="App">
+      <AddTask />
+      <ListTask />
+      <div>
+        <input type="radio" name="btn" id="all" onChange={handleSelect}/>
+        <label for="all" >SHOW  ALL</label>
+      </div>
+      <div>
+        <input type="radio" name="btn" id="true" onChange={handleSelect}/>
+        <label for="true" >IS  DONE</label>
+      </div>
+      <div>
+        <input type="radio" name="btn" id="false" onChange={handleSelect} />
+        <label for="false" >NOT  DONE</label>
+      </div>
+    </div>
+  );
+}
+export default App;
